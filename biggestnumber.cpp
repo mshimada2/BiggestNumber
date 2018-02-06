f742b8b48f6b8fc14e754bbab12e951a53630533
mshimada <mshimada2@mail.csuchico.edu>
2018-02-05 17:59:15 -0800

    final version

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,17 +1,38 @@
-//Author:
+//Author: McDermott Liam Shimada 
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
+
+int number1;
+int number2;
+int biggest;
 
 int main()
 {
   
   cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  cin>>number1;
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Please enter another whole number:\n";
+  cin>>number2;
+    
+    if (number1 > number2){
+
+    biggest = number1;
+
+
+    }
+
+    else{
+
+    biggest = number2;
+
+    }
+
+
+  cout<<"Of those two numbers, the biggest is: "<<biggest<<endl;
   cout<<endl<<"Thank you for playing.\n";
 
+  
   return 0;
 }
